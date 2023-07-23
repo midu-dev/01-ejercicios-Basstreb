42b4becfdcc659b5384c839b8e6dc0524f0eaf96
Daniel Lozano Campano <danlozcam@gmail.com>
2023-07-24 01:54:14 +0200

    Exercise 2: Finished writeFile

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// Ejercicio 2
-async function writeFile (filePath, data, callback) {
+const fs = require("node:fs/promises");
+const path = require("node:path");
 
+// Ejercicio 2
+async function writeFile(filePath, data, callback) {
+  const dir = path.dirname(filePath);
+  await fs.mkdir(dir, { recursive: true });
+  await fs.writeFile(filePath, data).then(() => callback(null)).catch((err) => callback(err));
 }
 
 // Ejercicio 3
-async function readFileAndCount (word, callback) {
-
+async function readFileAndCount(word, callback) {
 }
 
 module.exports = {
   writeFile,
-  readFileAndCount
-}
+  readFileAndCount,
+};
