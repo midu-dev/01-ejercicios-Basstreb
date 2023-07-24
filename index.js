fd5d31dda3bb3c38c629a8a19539a6c86a6f3353
Daniel Lozano Campano <danlozcam@gmail.com>
2023-07-24 02:46:16 +0200

    Exercise 3: Finished readFileAndCount

--- index.js
+++ index.js
@@ -5,11 +5,34 @@ const path = require("node:path");
 async function writeFile(filePath, data, callback) {
   const dir = path.dirname(filePath);
   await fs.mkdir(dir, { recursive: true });
-  await fs.writeFile(filePath, data).then(() => callback(null)).catch((err) => callback(err));
+  await fs
+    .writeFile(filePath, data)
+    .then(() => callback(null))
+    .catch((err) => callback(err));
 }
 
 // Ejercicio 3
 async function readFileAndCount(word, callback) {
+  const folder = process.argv[2];
+  let count = 0;
+
+  if (!word) {
+    return callback(new Error("No se ha especificado la palabra a buscar"));
+  }
+
+  if (!folder) {
+    return callback(new Error("No se ha especificado el path del archivo"));
+  }
+
+  await fs
+    .readFile(folder, "utf-8")
+    .then((text) => {
+      count = text.split(word).length - 1;
+      callback(null, count);
+    })
+    .catch(() => {
+      callback(null, count);
+    });
 }
 
 module.exports = {
